9612e5327da2c3348ea879f124c7996e4547aff2
Shaghayegh <87799046+LimeQuote@users.noreply.github.com>
2024-07-18 12:52:05 +0200

    Test for chrome_driver

--- chrome_brows_sim.py
+++ chrome_brows_sim.py
@@ -1,3 +1,5 @@
+#This program checks if chromedriver is running using the Selenium library.
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
