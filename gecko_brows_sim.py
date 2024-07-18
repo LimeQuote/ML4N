f4f2f3ef73ad15e0860e05af34bc0355bea6611c
Shaghayegh <87799046+LimeQuote@users.noreply.github.com>
2024-07-18 12:37:05 +0200

    Test for gecko_driver

--- gecko_brows_sim.py
+++ gecko_brows_sim.py
@@ -1,3 +1,5 @@
+#This program checks if geckodriver is running using the Selenium library.
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
