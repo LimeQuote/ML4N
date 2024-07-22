f4a3c971eb5ef24811274cbc36b6c36fe6bdd328
Shaghayegh <87799046+LimeQuote@users.noreply.github.com>
2024-07-22 21:46:08 +0200

    perform search Chrome_Google_MachineLearning

--- run_tshark.bat
+++ run_tshark.bat
@@ -2,7 +2,7 @@
 
 REM Run tshark to capture packets
 cd C:\Program Files\Wireshark
-tshark -i Wi-Fi -f "tcp port 80 or tcp port 443" -w C:\Users\Shaghayegh\captured_packets.pcap
+tshark -i Wi-Fi -f "tcp port 80 or tcp port 443" -w E:\browser_website_query_50rep\Chrome_Google_MachineLearning.pcap
 
 REM Check if tshark command was successful
 IF %ERRORLEVEL% NEQ 0 (
