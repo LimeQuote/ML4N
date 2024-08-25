b1d3b433125463659b497765c7f9a9daba9999ad
Shaghayegh <87799046+LimeQuote@users.noreply.github.com>
2024-08-25 17:56:39 +0200

    Description added

--- pcap_to_csv.bat
+++ pcap_to_csv.bat
@@ -1,4 +1,32 @@
 @echo off
+
+REM ------------------------------------------------------------------------------
+REM Description:
+REM This batch file automates the process of extracting specific network traffic 
+REM fields from a .pcap file using T-shark, the command-line version of Wireshark. 
+REM It filters for TCP, HTTP, and TLS handshake packets and saves the extracted 
+REM data fields into a CSV file. The file name for both input and output is based 
+REM on the provided input base name.
+REM 
+REM Fields Extracted:
+REM - tcp.stream: Identifies TCP streams.
+REM - ip.src: Source IP address.
+REM - ip.dst: Destination IP address.
+REM - tcp.srcport: Source TCP port.
+REM - tcp.dstport: Destination TCP port.
+REM - tcp.flags: TCP flags.
+REM - frame.len: Length of the Ethernet frame.
+REM - tcp.len: Length of the TCP segment.
+REM - tcp.window_size_value: TCP window size.
+REM - tcp.seq: TCP sequence number.
+REM - ip.ttl: IP time to live.
+REM - tcp.ack: TCP acknowledgment number.
+REM 
+REM Author: Shaghayegh Samadzadeh
+REM Date: July 2024
+REM ------------------------------------------------------------------------------
+
+
 REM Check if the correct number of arguments is provided
 if "%~1"=="" (
     echo Usage: %0 ^<input_base_name^>
